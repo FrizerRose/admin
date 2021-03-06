1384522d35a490b07dfddfe7ca9bd63629f27582
JurajMarkesic <jmarkesic@tvz.hr>
2021-03-06 19:20:50 +0100

    Added companyBeingEdited check before rendering the edit component.

--- src/components/CustomerListEdit.vue
+++ src/components/CustomerListEdit.vue
@@ -2,24 +2,24 @@
   <modal v-if="customer">
     <a-form-item label="Customer name">
       <a-input
-        v-model:value="customer.name"
+        v-model:value="formData.name"
         placeholder="Name"
       />
     </a-form-item>
     <a-form-item label="Customer slug">
       <a-input
-        v-model:value="customer.bookingPageSlug"
+        v-model:value="formData.bookingPageSlug"
         placeholder="Name"
       />
     </a-form-item>
     <a-form-item label="Customer email">
       <a-input
-        v-model:value="customer.contactEmail"
+        v-model:value="formData.contactEmail"
         placeholder="Name"
       />
     </a-form-item>
     <a-form-item label="Customer status">
-      <a-switch v-model:checked="customer.isPublic" />
+      <a-switch v-model:checked="formData.isPublic" />
     </a-form-item>
     <button @click="testfunc()" />
   </modal>
--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -2,21 +2,28 @@
   <common-layout>
     <h3>Customer list</h3>
     <CustomerListTable />
-    <CustomerListEdit />
+    <CustomerListEdit v-if="customer" />
   </common-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 import CommonLayout from '@/components/layout/CommonLayout.vue'; // @ is an alias to /src
 import CustomerListTable from '@/components/CustomerListTable.vue';
 import CustomerListEdit from '@/components/CustomerListEdit.vue';
+import { useStore } from '@/store';
 
 export default defineComponent({
   components: {
     CommonLayout,
     CustomerListTable,
     CustomerListEdit,
+  },
+  setup() {
+    const store = useStore();
+    const customer = computed(() => store.state.company.companyBeingEdited);
+
+    return { customer };
   },
 });
 </script>
